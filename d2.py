3628940847d8c7b708e5ececf6e42cc09f282687
EliBildman <eli@bildman.com>
2018-03-12 11:06:17 -0400

    hmmmmm sort points broken again

--- d2.py
+++ d2.py
@@ -12,13 +12,25 @@ def points_open(w, h, recs):
             points.append((x, y))
     return points
 
-def find_recs(w, h):
+def in_any(shapes, point):
+    for shape in shapes:
+        if shape.contains_point(point):
+            return True
+    return False
+
+def find_recs(w, h, max_size):
     recs = []
     open_points = points_open(w, h, recs)
     while len(open_points) > 0:
         point = open_points[randint(0, len(open_points) - 1)]
-        
-
+        end = point
+        while not in_any(recs, end) and end[0] - point[0] <= max_size and end[0] < w:
+            end = (end[0] + 1, end[1])
+        end = (end[0] - 1, end[1])
+        while not in_any(recs, end) and end[1] - point[1] <= max_size and end[1] < h:
+            end = (end[0], end[1] + 1)
+        end = (end[0], end[1] - 1)
+        recs.append(Rectangle(point, end))
 
 
 
@@ -28,8 +40,8 @@ h = 100
 img = Image.new("RGBA", (w, h), "white")
 pxs = img.load()
 
-r = Rectangle((25, 25), (75, 75))
-r.fill(pxs, (0,0,255))
-r.outline(pxs)
+recs = find_recs(w, h, w/2)
+for rec in recs:
+    rec.outline(pxs)
 
 img.save("test.png", "PNG")
--- testing.py
+++ testing.py
@@ -1,15 +1,2 @@
-class C1(object):
-    def __init__(self, x):
-        self.x = x
-
-    def print_thing(self):
-        print self.x
-
-class C2(C1):
-
-    def __init__(self):
-        C1.__init__(self, 20)
-
-
-y = C2()
-y.print_thing()
+x = (0,0)
+x[0] += 1
