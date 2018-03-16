3d5731f73e675856bae022b95ce88c7e0a7f65f5
EliBildman <eli@bildman.com>
2018-03-16 12:26:17 -0400

    working on d3

--- d3.py
+++ d3.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import pallet_maker
 import shapes
 import random
@@ -5,26 +6,37 @@ import random
 def rand_squares(w, h, num, min_size, max_size):
     sqrs = []
     for i in range(num):
-        center = (random.randint(0, w - 1), random.randint(0, h - 1)) # why doesnt this language have do while
+        center = (random.randint(min_size, w - min_size - 1), random.randint(min_size, h - min_size - 1)) # why doesnt this language have do while
         side = random.randint(min_size, max_size)
         while not valid_square(w, h, center, side):
-            center = (random.randint(0, w - 1), random.randint(0, h - 1))
             side = random.randint(min_size, max_size)
         sqrs.append(shapes.Square(center, side))
     return sqrs
 
+def num_contains(point, shapes):
+    num = 0
+    for shape in shapes:
+        if shape.contains_point(point):
+            num += 1
+    return num
 
 def valid_square(w, h, center, side):
     r = side / 2
     return 0 <= center[0] - r and center[0] + r < w and 0 <= center[1] - r and center[1] + r < h
 
-w = 500
-h = 500
+w = 1920
+h = 1080
 
-img = Image.new("RGBA", (w, h), "white")
+img = Image.new("RGBA", (w, h), "black")
 pxs = img.load()
 
-
-
+sqrs = rand_squares(w, h, 100, w / 10, w / 3)
+j = 0
+for sqr in sqrs:
+    j += 1
+    print j
+    for x in range(sqr.points[2][0], sqr.points[0][0]):
+        for y in range(sqr.points[2][1], sqr.points[0][1]):
+            pxs[x, y] = tuple(pxs[x,y][i] + 10 for i in range(3))
 
 img.save("test.png", "PNG")
